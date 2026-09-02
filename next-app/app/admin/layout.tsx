a38d1a4853b183c83cb7678cc49efe11b86b8700
photosrangel <rangelsantos1812@gmail.com>
2026-09-03 00:10:37 +0100

    Corrigir migração completa do painel Next.js

---
+++ next-app/app/admin/layout.tsx
@@ -0,0 +1,7 @@
+import '../admin-react/admin.css';
+
+export const dynamic = 'force-dynamic';
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
